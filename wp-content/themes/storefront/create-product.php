edfd3d5e7c7ba2e25690eec7ef3989f5af9162d2
Roman <chajkovskyj.roman@gmail.com>
2022-05-20 23:48:34 +0300

    woo prod
    Add prod type form

--- wp-content/themes/storefront/create-product.php
+++ wp-content/themes/storefront/create-product.php
@@ -3,6 +3,11 @@
 <form method="post" action="">
     <p>Title<input type="text" name="post_title"></p>
     <p>Price<input type="text" name="regular_price"></p>
+    <select name="_product_type"> 
+    <option value="Rare" selected>Rare</option>
+    <option value="Frequent">Frequent</option>
+    <option value="Unusual">Unusual</option>
+    </select>
     
     <input type="submit" value="click" name="submit"> <!-- assign a name for the button -->
 </form>
@@ -10,8 +15,12 @@
 <?php
 function display(){
     $post_title = $_POST['post_title'];
-    $post_price = $_POST['regular_price'];
+    $product_price = $_POST['regular_price'];
+    $product_type = $_POST['_product_type'];
     // echo "hi ".$_POST["studentname"];
+
+
+   
 
     $post_args = array(
         'post_title' => $post_title, // The product's Title
@@ -25,8 +34,9 @@ function display(){
     if ( ! empty( $post_id ) && function_exists( 'wc_get_product' ) ) {
         $product = wc_get_product( $post_id );
         $product->set_sku( $post_id ); // Generate a SKU with a prefix. (i.e. 'pre-123') 
-        $product->set_regular_price( $post_price ); // Be sure to use the correct decimal price
+        $product->set_regular_price( $product_price ); // Be sure to use the correct decimal price
         $product->set_category_ids( array( 16, 17 ) ); // Set multiple category ID's.
+        update_post_meta( $post_id, '_product_type', esc_attr( $product_type ) );
         $product->save(); // Save/update the WooCommerce order object.
     }
     
